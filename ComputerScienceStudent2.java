962e3e1ee89fbdd7d05731468bf7ec0261f1badd
abnae <ab.naemi@gmail.com>
2023-03-08 13:22:43 +0100

    Added a abstract class

---
+++ ComputerScienceStudent2.java
@@ -0,0 +1,9 @@
+public class ComputerScienceStudent2 extends  Student2{
+
+
+    public String getCourse (){
+        return "Computer Science";
+    }
+
+
+}
---
+++ Student2.java
@@ -0,0 +1,7 @@
+public abstract class Student2 {
+    abstract String getCourse ();
+
+    public String getName (){
+        return "test";
+    }
+}
