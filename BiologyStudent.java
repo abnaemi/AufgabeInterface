5a4ad6e337113e8ad237193754a2f2be0586552f
abnae <ab.naemi@gmail.com>
2023-03-08 10:54:16 +0100

    Added methods.

--- BiologyStudent.java
+++ BiologyStudent.java
@@ -3,7 +3,23 @@ public class BiologyStudent implements Citizen {
     String adress;
     int identityCardNumber;
 
+    public BiologyStudent() {
+    }
 
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setIdentityCardNumber(int identityCardNumber) {
+        this.identityCardNumber = identityCardNumber;
+    }
+
+    public BiologyStudent(String adress, int identityCardNumber) {
+        this.adress = adress;
+        this.identityCardNumber = identityCardNumber;
+
+
+    }
 
     @Override
     public String getAdress() {
@@ -13,5 +29,29 @@ public class BiologyStudent implements Citizen {
     @Override
     public int getIdentityCardNumber() {
         return identityCardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BiologyStudent that)) return false;
+
+        if (getIdentityCardNumber() != that.getIdentityCardNumber()) return false;
+        return getAdress() != null ? getAdress().equals(that.getAdress()) : that.getAdress() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAdress() != null ? getAdress().hashCode() : 0;
+        result = 31 * result + getIdentityCardNumber();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "BiologyStudent{" +
+                "adress='" + adress + '\'' +
+                ", identityCardNumber=" + identityCardNumber +
+                '}';
     }
 }
--- ComputerScienceStudent.java
+++ ComputerScienceStudent.java
@@ -3,6 +3,23 @@ public class ComputerScienceStudent implements  Citizen{
     String adress;
     int identityCardNumber;
 
+
+    public ComputerScienceStudent() {
+    }
+
+    public ComputerScienceStudent(String adress, int identityCardNumber) {
+        this.adress = adress;
+        this.identityCardNumber = identityCardNumber;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setIdentityCardNumber(int identityCardNumber) {
+        this.identityCardNumber = identityCardNumber;
+    }
+
     @Override
     public String getAdress() {
         return adress;
@@ -11,5 +28,29 @@ public class ComputerScienceStudent implements  Citizen{
     @Override
     public int getIdentityCardNumber() {
         return identityCardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ComputerScienceStudent that)) return false;
+
+        if (getIdentityCardNumber() != that.getIdentityCardNumber()) return false;
+        return getAdress() != null ? getAdress().equals(that.getAdress()) : that.getAdress() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAdress() != null ? getAdress().hashCode() : 0;
+        result = 31 * result + getIdentityCardNumber();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ComputerScienceStudent{" +
+                "adress='" + adress + '\'' +
+                ", identityCardNumber=" + identityCardNumber +
+                '}';
     }
 }
